4f0ecdf79c8edecbb5a6f633388f4c1071eed7c4
aboubacardiallo <aboubacardiallo594@outlook.fr>
2023-04-12 14:20:29 +0000

    unit tests was added

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DemoApplicationTests {
+	Calculator underTest = new Calculator();
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void itShouldAddTwoNumbers() {
+		// given
+		int numberOne= 20;
+		int numberTwo = 30;
+		// when
+		int result = underTest.add(numberOne, numberTwo);
+
+		// then
+		int expected = 50;
+		assertThat(result).isEqualTo(expected);
+    }
+
+    class Calculator {
+        int add(int a, int b) {
+            return a + b;
+        }
+    }
 
 }
