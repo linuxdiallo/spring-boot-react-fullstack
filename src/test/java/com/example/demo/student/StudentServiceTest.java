b60eb397f913ddb12470fa2af785663f4f84f140
aboubacardiallo <aboubacardiallo594@outlook.fr>
2023-04-13 13:03:25 +0000

    Unit & integration tests

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,10 +1,7 @@
 package com.example.demo.student;
-
 import com.example.demo.student.exception.BadRequestException;
 import com.example.demo.student.exception.StudentNotFoundException;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
